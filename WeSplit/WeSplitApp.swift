8eecb196195fc56791620900b0b7b2db60e2e4b6
Tamarawillis44 <techietl444@gmail.com>
2022-05-26 10:02:55 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tamara laraque on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
